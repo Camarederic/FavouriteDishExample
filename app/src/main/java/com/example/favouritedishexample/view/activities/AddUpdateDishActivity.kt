2db59b8117a42fbc2aca3dea821b7c762691cf1d
Camarederic <camarederic@mail.ru>
2022-08-20 19:18:25 +0300

    Implementing the Custom Dialog for Camera and Gallery selection

--- app/src/main/java/com/example/favouritedishexample/view/activities/AddUpdateDishActivity.kt
+++ app/src/main/java/com/example/favouritedishexample/view/activities/AddUpdateDishActivity.kt
@@ -1,14 +1,17 @@
 package com.example.favouritedishexample.view.activities
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import com.example.favouritedishexample.R
 import com.example.favouritedishexample.databinding.ActivityAddUpdateDishBinding
+import com.example.favouritedishexample.databinding.DialogCustomImageSelectionBinding
 
 // 10) Создаем активити
-class AddUpdateDishActivity : AppCompatActivity(), View.OnClickListener { // 21) Добавляем View.OnClickListener
+class AddUpdateDishActivity : AppCompatActivity(),
+    View.OnClickListener { // 21) Добавляем View.OnClickListener
 
     private lateinit var mBinding: ActivityAddUpdateDishBinding
 
@@ -20,15 +23,17 @@ class AddUpdateDishActivity : AppCompatActivity(), View.OnClickListener { // 21)
         // 25) Вызываем метод
         setupActionBar()
 
+        // 26) Создаем layout под названием dialog_custom_image_selection для камеры и фото
+
         // 23) Добавляем клик слушателя для картинки фото с плюсиком
         mBinding.imageViewAddDish.setOnClickListener(this)
     }
 
     // 24) Создаем метод для настройки тулбара для возврата назад
-    private fun setupActionBar(){
+    private fun setupActionBar() {
         setSupportActionBar(mBinding.toolbarAddDishActivity)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        mBinding.toolbarAddDishActivity.setNavigationOnClickListener{
+        mBinding.toolbarAddDishActivity.setNavigationOnClickListener {
             onBackPressed()
         }
     }
@@ -36,14 +41,39 @@ class AddUpdateDishActivity : AppCompatActivity(), View.OnClickListener { // 21)
     // 22) Добавляем метод onClick при нажатии на иконку фото с плюсиком
     override fun onClick(v: View?) {
         if (v != null) {
-            when(v.id){
-                R.id.imageViewAddDish ->{
-                    Toast.makeText(this, "You have clicked the ImageView",
-                        Toast.LENGTH_SHORT).show()
+            when (v.id) {
+                R.id.imageViewAddDish -> {
+//                    Toast.makeText(this, "You have clicked the ImageView",
+//                        Toast.LENGTH_SHORT).show()
+                    // 29) Вызываем метод
+                    customImageSelectionDialog()
+
                     return
                 }
             }
         }
+    }
+
+    // 27) Создаем метод для выбора в диалоговом окне пользовательского изображения
+    private fun customImageSelectionDialog() {
+        // 28) Создаем объект Dialog
+        val dialog = Dialog(this)
+        val binding: DialogCustomImageSelectionBinding =
+            DialogCustomImageSelectionBinding.inflate(layoutInflater)
+        dialog.setContentView(binding.root)
+
+        // 31) Создаем клики слушателей на камеру и пфллерею
+        binding.textViewCamera.setOnClickListener {
+            Toast.makeText(this, "Camera Clicked", Toast.LENGTH_SHORT).show()
+            dialog.dismiss()
+        }
+        binding.textViewGallery.setOnClickListener {
+            Toast.makeText(this, "Gallery Clicked", Toast.LENGTH_SHORT).show()
+            dialog.dismiss()
+        }
+
+        // 30) Показываем диалоговое окно
+        dialog.show()
     }
 }
 
