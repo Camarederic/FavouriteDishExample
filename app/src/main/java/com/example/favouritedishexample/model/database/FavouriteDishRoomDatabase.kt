7eceb4cc8c9860b5a075ac6ab87c61befd459175
Camarederic <camarederic@mail.ru>
2022-08-25 19:33:54 +0300

    Setting up the ROOM database

---
+++ app/src/main/java/com/example/favouritedishexample/model/database/FavouriteDishRoomDatabase.kt
@@ -0,0 +1,34 @@
+package com.example.favouritedishexample.model.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.favouritedishexample.model.entities.FavouriteDish
+import kotlin.synchronized as synchronized1
+
+// 17.1) Создаем класс
+@Database(entities = [FavouriteDish::class], version = 1)
+abstract class FavouriteDishRoomDatabase : RoomDatabase() {
+
+    // 17.2) Создаем метод
+    companion object {
+
+        @Volatile
+        private var INSTANCE: FavouriteDishRoomDatabase? = null
+
+        fun getDatabase(context: Context): FavouriteDishRoomDatabase {
+            return INSTANCE ?: synchronized1(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    FavouriteDishRoomDatabase::class.java,
+                    "favourite_dish_database"
+                ).fallbackToDestructiveMigration()
+                    .build()
+                INSTANCE = instance
+                instance
+
+            }
+        }
+    }
+}
