2965896b4f6ec100e6b26ebff82a8797e5d01b64
Camarederic <camarederic@mail.ru>
2022-08-30 22:48:33 +0300

    Hide and show the bottom navigation bar with Animation

--- app/src/main/java/com/example/favouritedishexample/view/activities/MainActivity.kt
+++ app/src/main/java/com/example/favouritedishexample/view/activities/MainActivity.kt
@@ -3,8 +3,10 @@ package com.example.favouritedishexample.view.activities
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.favouritedishexample.R
@@ -12,22 +14,40 @@ import com.example.favouritedishexample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var mBinding: ActivityMainBinding
+
+    // 25.1) Создаем переменную для navController
+    private lateinit var mNavController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
 
-        val navView: BottomNavigationView = binding.navView
+        // 25.2) Удаляем эту строчку
+        //val navView: BottomNavigationView = mBinding.navView
 
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
+        mNavController = findNavController(R.id.nav_host_fragment)
+
         val appBarConfiguration = AppBarConfiguration(setOf(
-            R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications))
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+            R.id.navigation_all_dishes, R.id.navigation_favourite_dishes, R.id.navigation_random_dish))
+        setupActionBarWithNavController(mNavController, appBarConfiguration)
+        mBinding.navView.setupWithNavController(mNavController)
+    }
+
+    // 25.3) Имплементируем метод
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(mNavController,null)
+    }
+    // 25.4) Создаем метод чтобы спрятать BottomNavigation
+    fun hideBottomNavigationView(){
+        mBinding.navView.clearAnimation()
+        mBinding.navView.animate().translationY(mBinding.navView.height.toFloat()).duration = 300
+    }
+    // 25.5) Создаем метод для появления BottomNavigation
+    fun showBottomNavigationView(){
+        mBinding.navView.clearAnimation()
+        mBinding.navView.animate().translationY(0f).duration = 300
     }
 }
